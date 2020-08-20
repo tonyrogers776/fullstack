86a783c35453f0a5f929a92d9fc19c48f12def99
Tony Rogers <tonyrogers776@gmail.com>
2020-08-19 19:46:32 -0700

    added login_and_registration

---
+++ login_and_registration/login/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+import re
+import bcrypt
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+class UserManager(models.Manager):
+    def validate(self,form):
+        errors = {}
+        if len(form['first_name']) < 2:
+            errors['first_name'] = 'First Name must be at least 2 characters'
+
+        if len(form['last_name']) < 2:
+            errors['last_name'] = 'Last Name must be at least 2 characters'
+
+        if not EMAIL_REGEX.match(form['email']):
+            errors['email'] = 'Invalid email address'
+
+        email_check = self.filter(email=form['email'])
+
+        if email_check:
+            errors['email'] = 'Email is already in use'
+
+        if len(form['password']) < 8:
+            errors['password'] = 'Password must be at least 8 characters'
+
+        if form['password'] != form['confirm-pw']:
+            errors['password'] = 'Passwords do not match'
+
+        return errors
+
+    def register(self, form):
+        pw = bcrypt.hashpw(form['password'].encode(), bcrypt.gensalt()).decode()
+        return self.create(
+            first_name = form['first_name'],
+            last_name = form['last_name'],
+            email = form['email'],
+            password = pw,
+        )
+
+    def authenticate(self, email, password):
+        users = self.filter(email=email)
+        if not users:
+            return False
+
+        user = users[0]
+        return bcrypt.checkpw(password.encode(), user.password.encode())
+
+            
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    confirm_pw = models.CharField(max_length=255)
+
+    objects = UserManager()
+
+# Create your models here.
